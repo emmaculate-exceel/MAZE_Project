ec730a4e0e58f5fd7ea791ce4542100d27bf02a6
emmaculate-exceel <emmaculate2exceel@live.com>
2024-09-06 15:39:43 +0100

    header files

---
+++ header_ve.h
@@ -0,0 +1,24 @@
+/**
+ *  creating of standard header file and Macros
+ * 
+ **/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL.h>
+#include <assert.h>
+
+// macros
+
+#define BOOL u32
+#define TRUE 1
+#define FALSE 0
+#define SCREEN_WIDTH 1027
+#define SCREEN_HEIGHT 776
+
+// aliases
+
+typedef Uint32 u32;
+typedef Uint64 u64;
+typedef Sint32 s32;
+typedef Sint64 s64;
