692ef70b2b935db6ca6d952cea3d69cd9c3d7412
emmaculate-exceel <emmaculate2exceel@live.com>
2024-09-06 23:37:53 +0100

    added pixels to the display

--- screen_ve.c
+++ screen_ve.c
@@ -17,14 +17,25 @@ void display_sdl(void)
   SDL_Window *win = SDL_CreateWindow("Details",
 				     SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
 				     SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-
   assert(win);
+  
   SDL_Renderer *renderer = SDL_CreateRenderer(win, 0, SDL_RENDERER_SOFTWARE);
   SDL_Texture *screen = SDL_CreateTexture(renderer, SDLPIXELFORMAT_RGB888,
 					  SDL_TEXTUREACCESS_STREAMING,
 					  SCREEN_WIDTH, SCREEN_HEIGHT);
-
   assert(screen);
+  
   SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
   SDL_RenderClear(renderer);
-  
+  u32 *screen_pixels = (u32 *) calloc(SCREEN_WIDTH, SCREEN_HEIGHT * sizeof(u32));   
+  assert(screen_pixels);
+
+  for (int i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT; i++)
+    {
+      screen_pixels[i] = 0xFF000000;
+    }
+
+  SDL_UpdateTexture(screen, NULL, screen_pixels, SCREEN_WIDTH * sizeof(u32));
+  SDL_RenderClear(renderer);
+  SDL_RenderCopy(renderer, screen, NULL, NULL);
+  SDL_RenderPresent(renderer);
