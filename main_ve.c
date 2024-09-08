62a6ce9fd0d6e3d6a57df87a6bd256e93fb8a577
emmaculate-exceel <emmaculate2exceel@live.com>
2024-09-08 17:14:19 +0100

    added functions to my main file

--- main_ve.c
+++ main_ve.c
@@ -1,9 +1,14 @@
 #include "header_ve.h"
-
+/**
+ * main : the main programme for handling user text input,
+ * text formatting including users input and output
+ *
+ *
+ */
 int main(int argc, char *argv[])
 {
 
-
-
+  display_sdl();
+  
   return 0;
 }
