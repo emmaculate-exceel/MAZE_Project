f9ffbd34dec2d1a7b5ba4a6c285eefdc95a22224
emmaculate-exceel <emmaculate2exceel@live.com>
2024-09-20 19:27:05 +0100

    my secondary programme

---
+++ Timer/README.md
@@ -0,0 +1,3 @@
+This is a timer built using the C programming language
+
+program manual will be discussed in the below sections of this readme file
---
+++ Timer/timer.h
@@ -0,0 +1,11 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+/// standard header files
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <stdio.h>
+#include <string.h>
+//#include <sstream>
+
+
