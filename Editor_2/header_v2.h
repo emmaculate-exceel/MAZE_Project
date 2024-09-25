0ad5740033eea13ddc27f72c2e942cba04353e40
emmaculate-exceel <emmaculate2exceel@live.com>
2024-09-25 18:02:49 +0100

    header files

---
+++ Editor_2/header_v2.h
@@ -0,0 +1,32 @@
+/**
+ * Header files for the program
+ * Functions for the program
+ * Macros for the program
+ * Variables
+ * Struct
+ */
+
+#ifndef HEADER_V2_H
+#define HEADER_V2_H
+
+/// Standard header files
+
+#include <string.h>
+#include <stdlib.h>
+#include <ncurses.h>
+#include <stdio.h>
+
+/// macros
+
+#define MAX_BUFFER_SIZE 1024
+
+
+
+/// functions
+
+void text_editor(const char *filename);
+
+//// variables for he 
+
+
+#endif /// end of line 
